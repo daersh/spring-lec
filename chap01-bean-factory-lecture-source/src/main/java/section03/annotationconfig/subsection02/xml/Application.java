6e7ecf458c9203d502f495e529bbc1047ed7b844
daersh <daersh@naver.com>
2024-02-13 11:16:50 +0900

    스캔 범위 지정 두가지 방식 (어노테이션방식, xml방식)

--- chap01-bean-factory-lecture-source/src/main/java/section03/annotationconfig/subsection02/xml/Application.java
+++ chap01-bean-factory-lecture-source/src/main/java/section03/annotationconfig/subsection02/xml/Application.java
@@ -1,7 +1,14 @@
 package section03.annotationconfig.subsection02.xml;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class Application {
     public static void main(String[] args) {
-        
+        ApplicationContext ac = new GenericXmlApplicationContext("section03/annotationconfig/subsection02/xml/spring-context.xml");
+        String[] beanNames = ac.getBeanDefinitionNames();
+        for(String beanName: beanNames){
+            System.out.println("beanName = " + beanName);
+        }
     }
 }
